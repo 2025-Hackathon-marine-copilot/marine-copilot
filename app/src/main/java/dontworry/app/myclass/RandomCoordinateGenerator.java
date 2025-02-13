d73e7de2e0182de9aa667f271ffdb76d6fc0e688
hardjava <fkdlcn123@kumoh.ac.kr>
2025-02-14 01:15:56 +0900

    add map function

---
+++ app/src/main/java/dontworry/app/myclass/RandomCoordinateGenerator.java
@@ -0,0 +1,22 @@
+package dontworry.app.myclass;
+
+import lombok.Getter;
+
+import java.util.Random;
+
+@Getter
+public class RandomCoordinateGenerator {
+    double lat1 = 35.08346392459766, lng1 = 129.094934463501;
+    double lat2 = 35.08806429179801, lng2 = 129.1026592254639;
+
+    public double[] getRandomCoordinate() {
+        Random random = new Random();
+        double t = random.nextDouble(); // 0 ~ 1 사이의 랜덤 값
+
+        // 랜덤한 비율(t)에 따라 두 점 사이의 위치 계산
+        double randomLat = (1 - t) * lat1 + t * lat2;
+        double randomLng = (1 - t) * lng1 + t * lng2;
+
+        return new double[]{randomLat, randomLng};
+    }
+}
