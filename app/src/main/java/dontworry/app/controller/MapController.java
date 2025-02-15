2d561f1be9130e5e6cb551f416595ef6a81497da
hardjava <fkdlcn123@kumoh.ac.kr>
2025-02-16 02:42:27 +0900

    add port weather info

--- app/src/main/java/dontworry/app/controller/MapController.java
+++ app/src/main/java/dontworry/app/controller/MapController.java
@@ -1,8 +1,11 @@
 package dontworry.app.controller;
 
 import dontworry.app.domain.ShipInfo;
+import dontworry.app.domain.Weather;
 import dontworry.app.service.AnchoredShipService;
 import dontworry.app.service.MovingShipService;
+import dontworry.app.service.PortWeatherService;
+import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -16,20 +19,24 @@ import java.util.List;
 public class MapController {
     private final AnchoredShipService anchoredShipService;
     private final MovingShipService movingShipService;
+    private final PortWeatherService portWeatherService;
 
-    public MapController(final AnchoredShipService anchoredShipService, final MovingShipService movingShipService){
+    public MapController(final AnchoredShipService anchoredShipService, final MovingShipService movingShipService, final PortWeatherService portWeatherService){
         this.anchoredShipService = anchoredShipService;
         this.movingShipService = movingShipService;
+        this.portWeatherService = portWeatherService;
     }
 
     @GetMapping
-    public String showMap(@ModelAttribute("currentShip") ShipInfo currentShip, Model model){
+    public String showMap(@ModelAttribute("currentShip") ShipInfo currentShip, Model model) throws Exception {
         List<ShipInfo> anchoredShipInfo = anchoredShipService.findAllAnchoredShipInfo();
         List<ShipInfo> movingShipInfo = movingShipService.findAllMovingShipInfo();
+        Weather weatherInfo = portWeatherService.getWeatherInfo();
 
         model.addAttribute("anchoredShips", anchoredShipInfo);
         model.addAttribute("movingShips", movingShipInfo);
         model.addAttribute("currentShip", currentShip);
+        model.addAttribute("weatherInfo", weatherInfo);
 
         return "map/map";
     }
---
+++ app/src/main/java/dontworry/app/service/PortWeatherService.java
@@ -0,0 +1,47 @@
+package dontworry.app.service;
+
+import dontworry.app.domain.Weather;
+import org.springframework.stereotype.Service;
+import java.net.*;
+import java.io.*;
+
+@Service
+public class PortWeatherService {
+    public Weather getWeatherInfo() {
+        String apiUrl = "https://apihub.kma.go.kr/api/typ01/url/sea_obs.php?tm=&stn=516090&help=0&authKey=a8j-o_bXStqI_qP21yraYg";
+        String extractedData = "";
+
+        try {
+            URL url = new URL(apiUrl);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+            con.setRequestMethod("GET");
+            con.setRequestProperty("Content-Type", "application/json");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String line;
+            int lineCount = 0;
+
+            while ((line = in.readLine()) != null) {
+                if (lineCount == 3) { // 두 번째 줄
+                    extractedData = line.trim();
+                    break;
+                }
+                lineCount++;
+            }
+            in.close();
+        } catch (Exception e) {
+            System.out.println("Error..");
+            return null;
+        }
+
+        String[] arr = extractedData.split(",");
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = arr[i].trim();
+        }
+
+        Weather weather = new Weather(arr[1], arr[6], arr[7], arr[8], arr[9], arr[10], arr[11], arr[12]);
+
+        return weather;
+    }
+}
