9cf7b7636324dccbbc8aba4799f09fe6e3bb045f
hardjava <fkdlcn123@kumoh.ac.kr>
2025-02-15 18:53:40 +0900

    schema added

---
+++ app/src/main/resources/hi.sql
@@ -0,0 +1,17 @@
+insert into ships(height, width, date_created, last_updated, imonumber, name)
+values (50, 200, now(), now(),'9241082', 'Going Merry');
+
+insert into ships(height, width, date_created, last_updated, imonumber, name)
+values (55, 250, now(), now(),'9241061', 'Sunny');
+
+insert into ships(height, width, date_created, last_updated, imonumber, name)
+values (60, 300, now(), now(),'123456', 'Titanic');
+
+insert into entering_ships(x, y, date_created, last_updated, ship_seq_id_id)
+values (, , now(),now(), 1)
+
+insert into entering_ships(x, y, date_created, last_updated, ship_seq_id_id)
+values (, , now(),now(), 2)
+
+insert into entering_ships(x, y, date_created, last_updated, ship_seq_id_id)
+values (, , now(),now(), 3)
