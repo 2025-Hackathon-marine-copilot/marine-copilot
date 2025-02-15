5f3847c9eab55e2006b3ee311beb6fe179e660f7
hardjava <fkdlcn123@kumoh.ac.kr>
2025-02-16 03:19:12 +0900

    portWeatherInfo

--- app/src/main/java/dontworry/app/controller/MapController.java
+++ app/src/main/java/dontworry/app/controller/MapController.java
@@ -6,6 +6,7 @@ import dontworry.app.service.AnchoredShipService;
 import dontworry.app.service.MovingShipService;
 import dontworry.app.service.PortWeatherService;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
