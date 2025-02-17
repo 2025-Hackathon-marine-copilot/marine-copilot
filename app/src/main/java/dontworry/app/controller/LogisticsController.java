cea01a22ad97594c7a790e6eff073c5b38f58463
hardjava <fkdlcn123@kumoh.ac.kr>
2025-02-17 21:56:16 +0900

    logistics inspection

--- app/src/main/java/dontworry/app/controller/LogisticsController.java
+++ app/src/main/java/dontworry/app/controller/LogisticsController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/logistics")
 public class LogisticsController {
-
+    //temp
     @GetMapping("/inspection")
     public String index(){
         return "logistics/index";
